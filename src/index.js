7fe5406680fd159ce54f1e7386f70d42b121a388
André Lins <andre.lins@softplan.com.br>
2019-09-15 10:26:08 -0300

    Remove statefull

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import defineElement from "./custom-element";
 import Button from "@material-ui/core/Button";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Button variant="contained" color="primary">
-        Hello World!
-      </Button>
-    );
-  }
-}
+const App = () => (
+  <Button variant="contained" color="primary">
+    Hello World!
+  </Button>
+);
 
 defineElement(App, "button-test", [], []);
